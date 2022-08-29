63a6132c2a6ac515aa0dde9fce571dc522ff47a4
Galdian <krzysztof.s.sieja@gmail.com>
2022-08-29 12:32:07 +0200

    version with a little bit of gui

--- main.py
+++ main.py
@@ -3,10 +3,133 @@ from src.tdd_stm.player_list import Player, PlayerList
 import itertools
 from tkinter import *
 from tkinter import messagebox
-
+from tkinter import ttk
 
 # CONSTANTS
 version = "0.8 beta"
+FONT_NAME = "arial"
+
+
+# TABLE
+def update():
+    game_frame = Frame(window)
+
+    my_game = ttk.Treeview(game_frame)
+    game_frame.grid(column=1, row=1)
+
+    my_game['columns'] = (
+    'Standing', 'Starting_Number', 'Name', 'Surname', 'Rating', 'Points', 'SOS', 'SOSOS', 'SODOS', 'Opponents')
+
+    my_game.column("#0", width=0, stretch=NO)
+    my_game.column("Standing", anchor=CENTER, width=80)
+    my_game.column("Starting_Number", anchor=CENTER, width=80)
+    my_game.column("Name", anchor=CENTER, width=80)
+    my_game.column("Surname", anchor=CENTER, width=80)
+    my_game.column("Rating", anchor=CENTER, width=80)
+    my_game.column("Points", anchor=CENTER, width=60)
+    my_game.column("SOS", anchor=CENTER, width=60)
+    my_game.column("SOSOS", anchor=CENTER, width=60)
+    my_game.column("SODOS", anchor=CENTER, width=60)
+    my_game.column("Opponents", anchor=CENTER, width=120)
+
+    my_game.heading("#0", text="", anchor=CENTER)
+    my_game.heading("Standing", text="Standing", anchor=CENTER)
+    my_game.heading("Starting_Number", text="Starting Number", anchor=CENTER)
+    my_game.heading("Name", text="Name", anchor=CENTER)
+    my_game.heading("Surname", text="Surname", anchor=CENTER)
+    my_game.heading("Rating", text="Rating", anchor=CENTER)
+    my_game.heading("Points", text="Points", anchor=CENTER)
+    my_game.heading("SOS", text="SOS", anchor=CENTER)
+    my_game.heading("SOSOS", text="SOSOS", anchor=CENTER)
+    my_game.heading("SODOS", text="SODOS", anchor=CENTER)
+    my_game.heading("Opponents", text="Opponents", anchor=CENTER)
+
+    a = 0
+    for player in tournament_list.list_of_players:
+        my_game.insert(parent='', index='end', iid=a, text='',
+                       values=(f'{a + 1}', f'{player.starting_number}', f'{player.name}', f'{player.surname}',
+                               f'{player.rating}', f'{player.points}', f'{player.sos}', f'{player.sosos}',
+                               f'{player.sodos}', f'{player.opponents}'))
+        a += 1
+    my_game.grid(column=1, row=1)
+    left_label = Label(text=f"Rounds: {rounds} \nPlayers: {len(tournament_list.list_of_players)}")
+    left_label.grid(column=0, row=1)
+
+# START
+
+tournament_list = PlayerList()
+rounds = 5
+# tournament_list.add_player("Test", "test2",1900)
+
+# COMMANDS
+
+def addp_add():
+    name = name_entry.get()
+    surname = surname_entry.get()
+    rating = rating_entry.get()
+    tournament_list.add_player(name, surname, rating)
+    addp.destroy()
+    update()
+    print(tournament_list.list_of_players)
+
+def rmvp_rem():
+    stn = stn_entry.get()
+    player_to_delete = tournament_list.find_player_by_starting_number(stn)
+    del tournament_list.list_of_players[player_to_delete-1]
+    rmvp.destroy()
+    update()
+
+def remove_player_window():
+    global rmvp
+    rmvp = Toplevel(window)
+    rmvp.config(padx=50, pady=50)
+    rmvp.title('Remove player')
+    stn_label = Label(rmvp, text="Starting number:")
+    stn_label.grid(column=0, row=0)
+
+    global stn_entry
+    stn_entry = Entry(rmvp, width=40)
+    stn_entry.grid(column=1, row=0)
+
+    addp_button = Button(rmvp, text="Remove player", width=20, command=rmvp_rem)
+    addp_button.grid(column=0, row=1, columnspan=2)
+
+    update()
+
+def add_player_window():
+    global addp
+    addp = Toplevel(window)
+    addp.config(padx=50, pady=50)
+    addp.title('Add player')
+    name_label = Label(addp, text="Name:")
+    name_label.grid(column=0, row=0)
+
+    global name_entry
+    name_entry = Entry(addp, width=40)
+    name_entry.grid(column=1, row=0)
+
+    surname_label = Label(addp, text="Surname:")
+    surname_label.grid(column=0, row=1)
+
+    global surname_entry
+    surname_entry = Entry(addp, width=40)
+    surname_entry.grid(column=1, row=1)
+
+    rating_label = Label(addp, text="Rating:")
+    rating_label.grid(column=0, row=2)
+
+    global rating_entry
+    rating_entry = Entry(addp, width=40)
+    rating_entry.grid(column=1, row=2)
+
+    addp_button = Button(addp, text="Add player", width=20, command=addp_add)
+    addp_button.grid(column=0, row=3, columnspan=2)
+
+    update()
+
+
+
+
 
 #UI
 
@@ -15,8 +138,28 @@ window.title("Shogi Tournament Manager")
 
 window.config(padx=50, pady=50)
 
+# canvas = Canvas(width=500, height=400)
+# logo = PhotoImage(file="logo2.png")
+# canvas.create_image(170, 100, image=logo, anchor = N)
+# canvas.grid(column=0, row=0)
+title_label = Label(text="Shogi Tournament Manager", font=(FONT_NAME, 40, "bold"), fg="red")
+title_label.grid(column=0, row=0, columnspan=2)
 
-window.mainloop()
+
+add_player_button = Button(text="Add player", width=20, command=add_player_window)
+add_player_button.grid(column=0, row=3)
+
+remove_player_button = Button(text="Remove player", width=20, command=remove_player_window)
+remove_player_button.grid(column=0, row=4)
+
+
+update()
+
+
+
+exit = False
+while not exit:
+    window.update()
 #
 # test = PlayerList()
 # # test.add_player("Janik", "Kruse", 1563)
